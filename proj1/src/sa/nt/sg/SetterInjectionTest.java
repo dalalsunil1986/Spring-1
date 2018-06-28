e7a681e0b156b9d9edc7defd65f1be887aefa777
karra sankar <karrasankar09@gmail.com>
2018-06-28 22:14:34 +0530

    SETTERINJECTION SPRING PROGRAM

---
+++ proj1/src/sa/nt/sg/SetterInjectionTest.java
@@ -0,0 +1,29 @@
+package sa.nt.sg;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import sa.nt.se.WishMessageGenerator;
+
+
+public class SetterInjectionTest {
+	public static void main(String[] args) {
+		Resource res=null;
+		BeanFactory factory=null;
+		Object obj=null;
+		WishMessageGenerator generator=null;
+		//location SpringBean wi file
+		res=new FileSystemResource("src/s/nt/wi/applicationContext.xml");
+		//Create IOC container(BeanFactrory)
+		factory=new XmlBeanFactory(res);
+		//get Bean(Main Bean)
+		obj=factory.getBean("wish");
+		//type casting 
+		generator=(WishMessageGenerator)obj;
+		//invoke business method
+		System.out.println("Message:::"+generator.generationWishMsg("Sankar"));
+		
+	}
+
+}
