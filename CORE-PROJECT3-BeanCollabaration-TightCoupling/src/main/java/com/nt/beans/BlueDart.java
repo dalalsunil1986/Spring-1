4c8f7015652e57a68f3ca5e8ff0712cdb08627c7
karra sankar <karrasankar158@gmail.com>
2018-09-15 14:06:00 +0530

    BeanCollaboration-TightCoubling

---
+++ CORE-PROJECT3-BeanCollabaration-TightCoupling/src/main/java/com/nt/beans/BlueDart.java
@@ -0,0 +1,11 @@
+package com.nt.beans;
+
+public class BlueDart {
+	
+	public  void deliver(int oid){
+		
+		System.out.println("BlueDart::delivering order whose order id::"+oid);
+		
+	}
+
+}
---
+++ CORE-PROJECT3-BeanCollabaration-TightCoupling/src/main/java/com/nt/beans/DTDC.java
@@ -0,0 +1,10 @@
+package com.nt.beans;
+
+public class DTDC {
+	
+	public  void deliver(int oid){
+		
+		System.out.println("DTDC::delivering order whose order id::"+oid);
+	}
+
+}
